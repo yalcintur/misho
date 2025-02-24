758cdd139827007170c2764fda3c627e9655250a
Yalçın Tür <yalcinturpg@gmail.com>
2025-02-23 23:25:49 -0800

    train yamls

--- eval.py
+++ eval.py
@@ -9,7 +9,7 @@ device = (
 )
 
 # Define the local path to your model
-local_model_path = "/data/yalcin/sft_output/checkpoint-2000"
+local_model_path = "/data/yalcin/sft_output_135-instruct/checkpoint-3000"
 
 
 # Load the model and tokenizer from the local path
@@ -19,13 +19,18 @@ tokenizer = AutoTokenizer.from_pretrained(local_model_path)
 # Set up the chat format
 model, tokenizer = setup_chat_format(model=model, tokenizer=tokenizer)
 
-prompt = "6 6 6 6"
+custom_eos_token = "."
+
+tokenizer.eos_token_id = tokenizer.convert_tokens_to_ids(custom_eos_token)
+model.config.eos_token_id = tokenizer.eos_token_id
+
+prompt = "4 4 4 4"
 
 messages = [{"role": "user", "content": prompt}]
 formatted_prompt = tokenizer.apply_chat_template(messages, tokenize=False)
 
 # Generate response
 inputs = tokenizer(formatted_prompt, return_tensors="pt").to(device)
-outputs = model.generate(**inputs, max_new_tokens=100, temperature=0)
+outputs = model.generate(**inputs, max_new_tokens=100, eos_token_id=tokenizer.eos_token_id, temperature=0.5)
 print("Before training:")
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
